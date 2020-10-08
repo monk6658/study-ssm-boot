cde836f0caddc1636325f558438a880fb0059d6a
monk6658 <monk6658@163.com>
2020-10-08 23:47:37 +0800

    2020-10-08 设计模式（动态代理[jdk、cglib]、观察者、责任链）

--- src/main/java/com/study/pattern/责任链/多拦截/Test.java
+++ src/main/java/com/study/pattern/责任链/多拦截/Test.java
@@ -7,11 +7,10 @@ import com.study.pattern.责任链.InterceptorJdkProxy;
 public class Test {
     public static void main(String[] args) {
 
-
+        // proxy1代理真实对象，proxy2代理proxy1，proxy3代理proxy2
         HelloWorld proxy1 = (HelloWorld) InterceptorJdkProxy.bind(new HelloWorldImpl(),"com.study.pattern.责任链.多拦截.Interceptor1");
         HelloWorld proxy2 = (HelloWorld) InterceptorJdkProxy.bind(proxy1,"com.study.pattern.责任链.多拦截.Interceptor2");
         HelloWorld proxy3 = (HelloWorld) InterceptorJdkProxy.bind(proxy2,"com.study.pattern.责任链.多拦截.Interceptor3");
-
         proxy3.sayHelloWorld();
     }
 }
